dd56fd25fe34e56a3afd61d2bfa779f9559c0571
tetianadoc <doctatyana1@gmail.com>
2021-09-30 19:50:47 +0200

    Add tests for add and delete operations.

--- service/src/test/java/com/gmail/doctatyana1/warehouse_api/service/WarehouseServiceImplTest.java
+++ service/src/test/java/com/gmail/doctatyana1/warehouse_api/service/WarehouseServiceImplTest.java
@@ -5,22 +5,39 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Add tests for {@link WarehouseServiceImpl}
+ * Add unit tests for {@link WarehouseServiceImpl}
  */
 
 
- final class WarehouseServiceImplTest {
-    private WarehouseService warehouseService=new WarehouseServiceImpl(10);
+final class WarehouseServiceImplTest {
+    private WarehouseService warehouseService = new WarehouseServiceImpl(10);
 
     @Test
-    void shouldCountGoods(){
+    void shouldCountGoods() {
         //Arrange
-        warehouseService=new WarehouseServiceImpl(20);
+        warehouseService = new WarehouseServiceImpl(20);
         // Act and Assert
         assertThat(warehouseService.count()).isEqualTo(20);
     }
 
+    @Test
+    void shouldAddNewGood() {
+        //Arrange
+        long numberOfGoodsBefore = warehouseService.count();
+        //Act
+        warehouseService.add();
+        //Assert
+        assertThat(warehouseService.count()).isEqualTo(numberOfGoodsBefore + 1);
+    }
 
-
+    @Test
+    void shouldRemoveGood() {
+        //Arrange
+        long numberOfGoodsBefore = warehouseService.count();
+        //Act
+        warehouseService.delete();
+        //Assert
+        assertThat(warehouseService.count()).isEqualTo(numberOfGoodsBefore - 1);
+    }
 
 }
